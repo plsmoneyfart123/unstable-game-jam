14d8d28924595559d206b09c3deb6514ec24c694
plsmoneyfart123 <plsmoneyfart123@gmail.com>
2026-08-15 21:58:35 +0200

    meterupdate

--- core unstable/Assets/meter_script.cs
+++ core unstable/Assets/meter_script.cs
@@ -5,6 +5,7 @@ using UnityEngine.UIElements;
 public class meter_script : MonoBehaviour
 {
     [SerializeField] private GameObject meter;
+    [SerializeField] private SpriteRenderer sprite;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private float growth;
 
@@ -12,6 +13,10 @@ public class meter_script : MonoBehaviour
     void FixedUpdate()
     {
         growth += 0.05f;
+        if (growth < 5)
+        {
+            sprite.color = Color.orange;
+        }
     }
     void Update()
     {
