b090fa0f5ce93ef65a050cbdbbde41197a7d0607
plsmoneyfart123 <plsmoneyfart123@gmail.com>
2026-08-15 17:17:07 +0200

    coolboy movement

---
+++ core unstable/Assets/move_script.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class move_script : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _Rb2D;
+    private float speed = 10f;
+    private float horizontal;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void FixedUpdate()
+    {
+        _Rb2D.linearVelocity = new Vector2 (horizontal * speed ,
+         _Rb2D.linearVelocityY); // y thing
+    }
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
+    }
+}
